9dcb64bf2879a2c63d625a47ebde852afeaa6f7f
fireman03151 <fireman03151@outlook.com>
2024-01-14 22:23:56 -0500

    #6 Repair the code(index,singup)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="423913375867-g5bdhnk3r026teranael9ceaaoa64e6b.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
