7dc8cbe58f7ada38cd8a139481940b9ef49d5340
fireman03151 <fireman03151@outlook.com>
2024-01-17 02:48:34 -0500

    #25 develope signUp_Style (30%)

--- frontend/src/pages/auth/auth.stype.js
+++ frontend/src/pages/auth/auth.stype.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import SignIn from '../../access/SignIn.png';
 import logo from '../../access/postJob/Logo.svg';
 import mainbg from '../../access/postJob/main.png';
+import logo_Signup from '../../access/logo_SignUp.png';
 
 const Auth_Style = styled.div`
 .Background_signUp {
@@ -13,6 +14,18 @@ const Auth_Style = styled.div`
   // background-attachment: initial;
   // background-position: center;
   background-size: cover;
+}
+.logo {
+  background: url(${logo});
+  width: 18.94px;
+  height: 23.09px;
+  color: #3B368D;
+}
+.logo_signUp {
+  background: url(${logo_Signup});
+  width: 18.94px;
+  height: 23.09px;
+  color: #3B368D;
 }
 `;
 
