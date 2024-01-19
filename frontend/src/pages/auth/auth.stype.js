3e8958b07b9f91aabf755bce523bf081b1df8715
fireman03151 <fireman03151@outlook.com>
2024-01-19 02:53:11 -0500

    #25 add styled-component.

--- frontend/src/pages/auth/auth.stype.js
+++ frontend/src/pages/auth/auth.stype.js
@@ -3,6 +3,8 @@ import SignIn from '../../access/SignIn.png';
 import logo from '../../access/postJob/Logo.svg';
 import mainbg from '../../access/postJob/main.png';
 import logo_Signup from '../../access/logo_SignUp.png';
+import google_icon from '../../access/google-icon.png';
+import face_icon from '../../access/facebook.png';
 
 const Auth_Style = styled.div`
 .Background_signUp {
@@ -26,6 +28,20 @@ const Auth_Style = styled.div`
   width: 18.94px;
   height: 23.09px;
   color: #3B368D;
+}
+.google-icon {
+  background: url(${google_icon});
+  background-repeat: no-repeat;
+  background-position: 145px 5px;
+  width: 20px;
+  height: 20px;
+}
+.facebook-icon {
+  background: url(${face_icon});
+  background-repeat: no-repeat;
+  background-position: 140px 5px;
+  width: 20px;
+  height: 20px;
 }
 `;
 
